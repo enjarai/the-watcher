1fc11e347cfec928202c6ab7b6c4e56286278aa1
enjarai <enjarai123@gmail.com>
2022-01-30 18:52:00 +0100

    uploading my work so far

---
+++ README.md
@@ -0,0 +1,6 @@
+# The Watcher
+
+This is a discord bot designed for technical Minecraft servers.
+Currently, its only feature is displaying the material list of any litematica files that get uploaded.
+
+Depends on the litematicparse library, which I am currently working on.
---
+++ cogs/litematics.py
@@ -0,0 +1,88 @@
+from functools import partial
+from os import path
+
+import nextcord.ui
+from nextcord.ext import commands
+from nextcord.ui import Button
+
+import litematicparse
+
+
+class MaterialListView(nextcord.ui.View):
+    def __init__(self, name, region, *, timeout=None, blocks=True, entities=True, inventories=False):
+        super().__init__(timeout=timeout)
+        self.opts = {"Blocks": blocks, "Entities": entities, "Inventories": inventories}
+        self.name = name
+        self.region = region
+
+        for opt in self.opts:
+            self.add_toggle(opt, opt, self.toggle)
+
+    def add_toggle(self, label, opt, callback):
+        item = Button(
+            label=label,
+            style=self.get_toggled_style(self.opts[opt])
+        )
+        item.opt = opt
+        item.callback = partial(callback, self, item)
+        item._view = self
+        # setattr(self, callback.__name__, item) # not sure if I need this, but it's here in case everything breaks
+        self.children.append(item)
+
+    async def toggle(self, view, button, interaction):
+        toggle = not self.opts[button.opt]
+        self.opts[button.opt] = toggle
+        button.style = self.get_toggled_style(toggle)
+        await self.update(interaction)
+
+    async def update(self, interaction):
+        embed = Litematics.get_material_list_embed(self.name, self.region, self.opts)
+        await interaction.response.edit_message(embed=embed, view=self)
+
+    @nextcord.ui.button(label="Delete", style=nextcord.ButtonStyle.danger)
+    async def delete(self, button, interaction):
+        await interaction.message.delete()
+
+    @staticmethod
+    def get_toggled_style(active):
+        return nextcord.ButtonStyle.primary if active else nextcord.ButtonStyle.secondary
+
+
+class Litematics(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+        # https://raw.githubusercontent.com/InventivetalentDev/minecraft-assets/1.18.1/assets/minecraft/lang/en_us.json
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        for attachment in message.attachments:
+            if attachment.filename.endswith('.litematic'):
+                await message.channel.trigger_typing()
+                print(f'Downloading and parsing {attachment.filename}')
+
+                file = path.join(self.bot.CONFIG['temp_directory'], 'schematic.litematic')
+                await attachment.save(file)
+
+                schematic = litematicparse.Litematic(file)
+
+                print(f'  Sending response')
+                for name, region in schematic.regions.items():
+                    view = MaterialListView(name, region)
+                    await message.reply(embed=self.get_material_list_embed(name, region, view.opts), view=view)
+
+    @staticmethod
+    def get_material_list_embed(name, region, opts):
+        materials = (
+            (region.block_count() if opts["Blocks"] else litematicparse.MaterialList()) +
+            (region.entity_count() if opts["Entities"] else litematicparse.MaterialList()) +
+            (region.inventory_count() if opts["Inventories"] else litematicparse.MaterialList())
+        )
+        formatted = str(materials)
+        embed = nextcord.Embed(title=name, description=f"```{formatted if formatted else 'Empty'}```")
+        return embed
+
+
+def setup(bot):
+    bot.add_cog(Litematics(bot))
